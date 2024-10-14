a469349b4b5d23ff80e3367835ea72e56261944d
Aditi-Chowdhuri <aditinirapada.2018@vitstudent.ac.in>
2024-10-14 01:15:27 -0400

    lexer.cpp modified

--- mlang_compile/src/lexer.cpp
+++ mlang_compile/src/lexer.cpp
@@ -78,4 +78,107 @@ Token Lexer::scanToken() {
     }
 }
 
+Token Lexer::scanIdentifierOrKeyword() {
+    
+    if (std::isdigit(input[position - 1])) {
+        std::string value(1, input[position - 1]);
+        LexerError::invalidIdentifier("input", line, column - 1, value);
+        return createToken(TokenType::UNKNOWN, value);
+    }
 
+    std::string value;
+    value += input[position - 1];  
+
+    while (std::isalnum(peek()) || peek() == '_') {
+        value += advance();
+    }
+
+    if (keywords.find(value) != keywords.end()) {
+        return createToken(TokenType::KEYWORD, value);
+    }
+
+    if (!std::isalpha(value[0]) && value[0] != '_') {
+        LexerError::invalidIdentifier("input", line, column - value.length(), value);
+    }
+
+    return createToken(TokenType::IDENTIFIER, value);
+}
+
+Token Lexer::scanNumber() {
+    std::string value;
+    value += input[position - 1];
+    bool hasDecimalPoint = false;
+
+    while (std::isdigit(peek()) || peek() == '.') {
+        if (peek() == '.') {
+            if (hasDecimalPoint) {
+                value += advance();
+                LexerError::invalidNumber("input", line, column - value.length(), value);
+                return createToken(TokenType::UNKNOWN, value);
+            }
+            hasDecimalPoint = true;
+        }
+        value += advance();
+    }
+
+    return createToken(TokenType::LITERAL, value);
+}
+
+Token Lexer::scanString() {
+    std::string value;
+    int startLine = line;
+    int startColumn = column - 1;
+
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') {
+            line++;
+            column = 1;
+        }
+        value += advance();
+    }
+
+    if (isAtEnd()) {
+        LexerError::unterminatedString("input", startLine, startColumn);
+        return createToken(TokenType::UNKNOWN, value);
+    }
+
+    advance();  // Consume the closing "
+    return createToken(TokenType::LITERAL, value);
+}
+
+
+void Lexer::skipWhitespace() {
+    while (true) {
+        char c = peek();
+        switch (c) {
+            case ' ': case '\r': case '\t':
+                advance();
+                break;
+            case '\n':
+                line++;
+                column = 1;
+                advance();
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+void Lexer::skipComment() {
+    while (peek() != '\n' && !isAtEnd()) advance();
+}
+
+std::string tokenTypeToString(TokenType type) {
+    switch (type) {
+        case TokenType::KEYWORD: return "KEYWORD";
+        case TokenType::IDENTIFIER: return "IDENTIFIER";
+        case TokenType::LITERAL: return "LITERAL";
+        case TokenType::OPERATOR: return "OPERATOR";
+        case TokenType::DELIMITER: return "DELIMITER";
+        case TokenType::COMMENT: return "COMMENT";
+        case TokenType::UNKNOWN: return "UNKNOWN";
+        case TokenType::END_OF_FILE: return "END_OF_FILE";
+        default: return "UNKNOWN";
+    }
+}
